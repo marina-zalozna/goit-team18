d4ca64ec453bae2ad35d182f91246e92f63965b7
marina-zalozna <106180626+marina-zalozna@users.noreply.github.com>
2022-08-13 16:01:03 +0300

    desktop

---
+++ js/main.js
@@ -0,0 +1,38 @@
+$(function() {
+
+    $('.reviews__container').slick({
+        dots: true,
+  infinite: false,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 320,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+    })
+}) 
